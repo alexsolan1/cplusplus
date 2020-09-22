beb79aa7c2412eeb58c1d450ba39a8793d06b4e3
alexsolan1 <alex.solan@gmail.com>
2020-09-22 17:55:45 +0300

    Update main.cc

--- main.cc
+++ main.cc
@@ -2,82 +2,68 @@
 #include <string.h>
 #include <cassert>
 
-
 using namespace std;
-constexpr unsigned INITIAL_CAPACITY = 16;
-
-// roundup tp next mult of INITIAL_CAPACITY
-inline unsigned calc_reserve(unsigned len) {
-    if (len % INITIAL_CAPACITY) {
-        len += INITIAL_CAPACITY - (len % INITIAL_CAPACITY);
-    }
-    return len;
-}
 
 class String {
 public:
     
-    String() {
-        m_capacity = INITIAL_CAPACITY;
-        m_data = new char[m_capacity];
-        m_len = 0;
-        m_data[m_len] = 0;
+    String() :
+        String("", 0)
+    { }
+
+    String(const char* data) :
+        String(data, strlen(data))
+    { }
+
+    String(const String& s) :
+        String(s.data(), s.length())
+    { }
+    
+    String(const char* data, size_t length) {
+        alloc_and_copy(data, length);
     }
     
-    String(const char* data, size_t len) {
-        m_capacity = calc_reserve(len + 1);
-        m_data = new char[m_capacity];
-        strncpy(m_data, data, len);
-        m_len = len;
-        m_data[m_len] = 0;
-    }
     
-    String(const char* data) {
-        m_len = strlen(data);
-        m_capacity = calc_reserve(m_len);
-        m_data = new char[m_capacity];
-        strncpy(m_data, data, m_len);
-        m_data[m_len] = 0;
+    String(String&& s) :
+        m_data(s.data()),
+        m_capacity(s.capacity()),
+        m_length(s.length()) 
+    { 
+        s.clean();
     }
-    
-    String(const String& s) {
-         m_capacity = s.capacity();
-         m_data = new char[m_capacity];
-         strncpy(m_data, s.data(), s.len());
-         m_len = s.len();     
-         m_data[m_len] = 0;
-    }
-    
+         
     String& operator=(const String& s) {
          // for x = x, ignoring more complicated cases where len/capacity differ
          if (m_data == s.data()) {
              return *this;
          }
-         delete[] m_data; 
-         m_capacity = s.capacity();
-         m_data = new char[m_capacity];
-         strncpy(m_data, s.data(), s.len());
-         m_data[s.len()] = 0;
-         m_len = s.len();
+         delete[] m_data;
+         alloc_and_copy(s.data(), s.length());
          return *this;
-     } 
-   
+    }
+    
+    String& operator=(String&& s) {
+        // for x = x, ignoring more complicated cases where len/capacity differ
+        if (m_data == s.data()) {
+            return *this;
+        }
+        m_data = s.data();
+        m_capacity = s.capacity();
+        m_length = s.length();
+        s.clean();
+    }
+        
     ~String() {
         delete[] m_data;
     }
     
     void reserve(unsigned capacity) {
         if (capacity < m_capacity) { return; }
-        m_capacity = capacity;
-        char* tmp = new char[m_capacity];
-        strcpy(tmp, m_data);
-        tmp[m_len] = 0;
-        delete[] m_data;
-        m_data = tmp;
+        reallocate(capacity);
     }
     
-    unsigned int len() const {
-        return m_len;
+    unsigned int length() const {
+        return m_length;
     }
 
     unsigned int capacity() const {
@@ -92,28 +78,18 @@ public:
         return m_data;
     }
  
-    char operator[] (unsigned int i) const {
-        assert(i < len());
+    char& operator[] (unsigned int i) const {
+        assert(i < length());
         return m_data[i];
     }
     
     String& operator+=(const String& s) {
-        unsigned oldlen = len();
-        m_len = len() + s.len();
-        if (m_len < m_capacity) {
-            // no need to re-alloc
-            strcpy(m_data + oldlen, s.data());
-            m_data[m_len] = 0;
-            return *this;
+        unsigned oldlen = length();
+        m_length = length() + s.length();
+        if (m_length + 1 >= m_capacity) {
+            reallocate(m_length + 1);
         }
-            
-        unsigned m_capacity = calc_reserve(m_len + 1);
-        char* tmp = new char[m_capacity];
-        strcpy(tmp, m_data);
-        strcpy(tmp + oldlen, s.data());
-        tmp[m_len] = 0;
-        delete[] m_data;
-        m_data = tmp;
+        add(s, oldlen);
         return *this;
     }
     
@@ -121,15 +97,50 @@ public:
         return m_data;
     }
      
-     char* end() {
-        return m_data + len();
-     }
- 
+    char* end() {
+        return m_data + length();
+    }
    
 private:
+    
+    void alloc_and_copy(const char* data, size_t length) {
+        m_capacity = length + 1;
+        m_length = length;
+        m_data = new char[m_capacity];
+        strncpy(m_data, data, length);
+        m_data[m_length] = 0;
+    }
+    
+    void reallocate(unsigned length) {
+        unsigned m_capacity = length;
+        char* tmp;
+        try {
+            tmp = new char[m_capacity];
+        }
+        catch (const std::bad_alloc& e) {
+            cerr << "Caught bad_alloc " << e.what() << endl;
+            throw(e);
+        }
+                
+        strcpy(tmp, m_data);
+        tmp[m_length] = 0;
+        delete[] m_data;
+        m_data = tmp;
+    }
+    
+    void add(const String& s, unsigned location) {
+        strcpy(m_data + location, s.data());
+        m_data[location + s.length()] = 0;
+    }
+    
+    void clean() {
+        m_data = nullptr;
+        m_length = 0;
+        m_capacity = 0;
+    }
     char* m_data;    
     unsigned m_capacity;
-    unsigned m_len;
+    unsigned m_length;
 };
 
 ostream& operator<<(ostream& os, const String& str) {
@@ -137,12 +148,15 @@ ostream& operator<<(ostream& os, const String& str) {
     return os;
 }
     
+String create_string() {
+    return "ALEX";
+}
 
 int main()
 {
     String s("Hi Alex");
     cout << s << endl;    
-    for(unsigned i = 0; i < s.len(); i++) {
+    for(unsigned i = 0; i < s.length(); i++) {
         cout << s[i] << " ";
     }
     cout << endl;
@@ -179,5 +193,17 @@ int main()
     
     my_copy.reserve(100);
     cout << "my copy after reserve: " << my_copy << " capacity: " << my_copy.capacity() << endl;
+    
+    // As requested....     
+    String s4 = "Hello!";
+    s4[s4.length() - 1] = '?';
+    assert('?' == s4[s4.length() - 1]);
+    
+    String s5(create_string());
+    cout << "s5(move ctor) " << s5 << endl;
+    
+    char* data = s5.data();
+    data[0] = 'B';
+    assert('B' == s5[0]);
     return 0;
 }
