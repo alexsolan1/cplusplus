66f9983e5785f9e8c3c72ba9f59d14efa7977e68
alexsolan1 <alex.solan@gmail.com>
2020-09-25 08:55:52 +0300

    Added new impl of optional

--- optional.cc
+++ optional.cc
@@ -1,5 +1,15 @@
 #include "optional.h"
 
+using namespace std;
+
+class MyClass {
+	public:
+	MyClass(int) {}
+	~MyClass() {
+		cout << "Dtor is called " << endl;
+	}
+};
+
 int main() {
 	Optional<int> opt;
 	assert(!opt);
@@ -8,5 +18,7 @@ int main() {
 	assert(optstring.get() == "Wedensday");
 	std::cout << "All is well. " << std::endl;	
 
+	Optional<MyClass> myopt;
+	assert(!myopt);
 	return 0;
 }
--- optional.h
+++ optional.h
@@ -1,31 +1,40 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <variant>
 
 template <class T>
 class Optional {
 public:
-	Optional() = default;
+	Optional() : m_value(nullptr), m_valid(false){
+	} 
+	
 	Optional(const T& t) :
-		m_value(t),
+		m_value(new (buf) T(t)),
 		m_valid(true)
 	{ }
+
+	~Optional() {
+		m_value->~T();
+	}
+	
 	operator bool() const {
 		return m_valid;
 	}
 
 	T& get() {
 		assert(m_valid);
-		return m_value;
+		return *m_value;
 	}
 		
 	const T& get() const {
 		assert(m_valid);
-		return m_value;
+		return *m_value;
 	}
 	
 private:
-	T m_value;
+	char buf[sizeof(T)];
+	T* m_value;
 	bool m_valid;
 };
 
