51821e7ab033c0ed906dd4f3ec0ab3ca2bfcbae6
alexsolan1 <alex.solan@gmail.com>
2020-09-30 16:36:25 +0300

    Added meta vector+for_each

--- mp_vector.cc
+++ mp_vector.cc
@@ -3,13 +3,23 @@
 #include "mp_vector.h"
 
 using namespace std;
+template <class T>
+struct ArrayOf3 {
+    using type = array<T, 3>;
+};
 
 int main() {
-    using mp_vec_t = mp_vector<bool, char, array<char, 17>>;
+    using mp_vec_t = mp_vector<size_t, bool, char, array<char, 17>>;
     static_assert(mp_vector<bool, char, double>::size == 3);
-    assert(sizeof(mp_vec_t::at<2>) == 17);
+    assert(sizeof(mp_vec_t::at<3>) == 17);
     using bigger_vec_t = mp_vec_t::push_back<array<char, 5>>;
-    assert(sizeof(bigger_vec_t::at<3>) == 5);
+    assert(sizeof(bigger_vec_t::at<4>) == 5);
+    
+    using arrays_of_t = for_each_t<mp_vec_t, ArrayOf3>;
+    static_assert(is_same_v<arrays_of_t::at<0>, ArrayOf3<size_t>::type>);
+    static_assert(is_same_v<arrays_of_t::at<1>, ArrayOf3<bool>::type>);
+    static_assert(is_same_v<arrays_of_t::at<2>, ArrayOf3<char>::type>);
+    static_assert(is_same_v<arrays_of_t::at<3>, ArrayOf3<array<char, 17>>::type>);
     cout << "All is well." << endl;
     return 0;
 }
--- mp_vector.h
+++ mp_vector.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <type_traits>
 
+namespace detail {
 template<size_t index, class... types> struct type_of_index { };
 template<size_t index, class T, class... types> 
 struct type_of_index<index, T, types...> {
@@ -15,14 +16,35 @@ struct type_of_index<0, T, types...> {
 template<> 
 struct type_of_index<0> {
 };
-
+} // end of namespace detil
 
 
 template <class... types>
 struct mp_vector {
     static constexpr size_t size = sizeof...(types);
     template <size_t index>
-    using at = typename type_of_index<index, types...>::type;
+    using at = typename detail::type_of_index<index, types...>::type;
     template <class T>
     using push_back = mp_vector<types..., T>;
 };
+
+template <class MetaVector, size_t index,  template<class> class MetaFunc>
+struct for_each_struct {
+    using cur_type = typename MetaVector::template at<index-1>;
+    using push_type = typename MetaFunc<cur_type>::type;
+    using prev_vector_t =  typename for_each_struct<MetaVector, index - 1, MetaFunc>::type;
+    using type = typename prev_vector_t::template push_back<push_type>;
+};
+
+template <class MetaVector, template<class> class MetaFunc>
+struct for_each_struct<MetaVector, 0, MetaFunc> {
+    using type = mp_vector<>;
+};
+template <class MetaVector, template<class> class MetaFunc>
+struct for_each {
+    using type = typename for_each_struct<MetaVector, MetaVector::size, MetaFunc>::type;
+};
+
+
+template <class MetaVector, template<class> class MetaFunc>
+using for_each_t = typename for_each<MetaVector, MetaFunc>::type;
